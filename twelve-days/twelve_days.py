88e1a8252725c870b18c5ae105e981b44783ae54
m1kal <mikal82@protonmail.com>
2020-02-03 07:48:05 +0100

    Twelve days v2

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -1,17 +1,15 @@
 days = ['first','second','third','fourth','fifth','sixth',
         'seventh','eighth','ninth','tenth','eleventh','twelfth']
-gifts = ['twelve Drummers Drumming', 'eleven Pipers Piping',
-         'ten Lords-a-Leaping', 'nine Ladies Dancing', 'eight Maids-a-Milking',
-         'seven Swans-a-Swimming', 'six Geese-a-Laying', 'five Gold Rings',
-         'four Calling Birds', 'three French Hens', 'two Turtle Doves',
-         'and a Partridge in a Pear Tree.']
+gifts = ['twelve Drummers Drumming', 'eleven Pipers Piping', 'ten Lords-a-Leaping',
+         'nine Ladies Dancing', 'eight Maids-a-Milking', 'seven Swans-a-Swimming',
+         'six Geese-a-Laying', 'five Gold Rings', 'four Calling Birds',
+         'three French Hens', 'two Turtle Doves', 'a Partridge in a Pear Tree']
+template = 'On the {} day of Christmas my true love gave to me: {}.'
 
 def recite(start_verse, end_verse):
-    return [verse(n-1) for n in range(start_verse, end_verse+1)]
+    return [verse(n) for n in range(start_verse-1, end_verse)]
 
-def verse(number):
-    day = days[number]
-    ans = 'On the ' + day + ' day of Christmas my true love gave to me: ' + \
-          ', '.join(gifts[12-number-1:])
-    return ans.replace('and ','') if number == 0 else ans
+def verse(idx):
+    return template.format(days[idx],
+        ', '.join(gifts[12-idx-1:-1] + [('and ' if idx else '') + gifts[-1]]))
 
